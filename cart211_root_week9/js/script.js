fe08e2352f7625921699da074fccee64c3ab7465
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-11-18 10:31:59 -0500

    Lab9 : Create dummy js page to test interactivity + text input

---
+++ cart211_root_week9/js/script.js
@@ -0,0 +1,82 @@
+let textInput = document.querySelector('#inputText').value
+
+let next = document.querySelector('#btn')
+
+let textOutput = document.querySelector('#output')
+
+const article = document.querySelector('#doc1');
+let articleMoved = true;
+
+const picture = document.querySelector('#doc2');
+let pictureMoved = true;
+
+
+//The text input will not be used in my final project, this is why I kind of did whatever.
+
+next.onclick = function() {
+  let textInput = document.querySelector('#inputText').value
+
+  if (textInput <= 1 || textInput >= 10) {
+    textOutput.textContent = `You think you're funny? I said 1 to 10. You're wrong.`
+  } else if (textInput >= 5) {
+    textOutput.textContent = `Guess what? Still wrong.`
+  } else if (textInput < 5) {
+    textOutput.textContent = `That's especially wrong.`
+  } else {
+    textOutput.textContent = `Hey, smarty pants. That's not even a number.`
+  }
+}
+
+
+// Colored squares are dummies for future articles/pictures from the brief
+
+article.onclick = function() {
+  if (!articleMoved) {
+    pictureSmall()
+    articleBig()
+  } else {
+    articleSmall()
+  }
+}
+
+picture.onclick = function() {
+  if (!pictureMoved) {
+    articleSmall()
+    pictureBig()
+  } else {
+    pictureSmall()
+  }
+}
+
+function articleBig() {
+  article.style.left = '50%'
+  article.style.top = '50%'
+  article.style.transform = 'scale(2)'
+  article.style.zIndex = 1
+  articleMoved = true
+}
+
+function articleSmall() {
+  article.style.left = '70%'
+  article.style.top = '10%'
+  article.style.transform = 'rotate(20deg)'
+  article.style.transform = 'scale(1)'
+  article.style.zIndex = -1
+  articleMoved = false
+}
+
+function pictureBig() {
+  picture.style.left = '50%'
+  picture.style.top = '50%'
+  picture.style.transform = 'scale(2)'
+  picture.style.zIndex = 1
+  pictureMoved = true
+}
+
+function pictureSmall() {
+  picture.style.left = '25%'
+  picture.style.top = '60%'
+  picture.style.transform = 'scale(1)'
+  picture.style.zIndex = -1
+  pictureMoved = false
+}
