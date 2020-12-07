81dfdedbd714a6bb5b46f020e43f76865c1ef6d9
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-12-07 17:31:36 -0500

    Created TheOne (from copy of proto-w13)

---
+++ TheOne/js/script.js
@@ -0,0 +1,20 @@
+//How to put html pages in array and make a random link selector with the front page logo?
+
+
+
+const dossier = document.querySelectorAll('.doc');
+let docMoved = true;
+
+
+// How to put docs in an array to make it possible to zoom out of them when click on
+let docs = ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8"]
+
+// console.log(dossier);
+dossier.forEach(function(c) {
+  console.log(c);
+  c.addEventListener('click', function() {
+
+    this.classList.toggle("zoom");
+    //make everything zoom out
+  });
+});
