ff68d01ddda53c94f41783d1f78695fda1e71517
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2020-12-07 18:33:07 -0500

    TheOne: added how its done transitions + wireframe + styleguide

--- TheOne/js/script.js
+++ TheOne/js/script.js
@@ -1,6 +1,6 @@
 //How to put html pages in array and make a random link selector with the front page logo?
 
-
+// -------- Zoom in / out on brief pages ----------
 
 const dossier = document.querySelectorAll('.doc');
 let docMoved = true;
@@ -18,3 +18,70 @@ dossier.forEach(function(c) {
     //make everything zoom out
   });
 });
+
+
+
+// =========== HOW IT's DONE page opennable sections =========
+// This code is based from the 'eventlist' files from root_day_9 shown in class
+
+//Styleguide opening
+const trigStyleguide = document.querySelector("#styleguide");
+const closerStyleguide = document.querySelector("#styleguideSec");
+
+trigStyleguide.addEventListener("click", openStyleguide);
+closerStyleguide.addEventListener("click", closeStyleguide);
+
+function openStyleguide() {
+  document.getElementById("styleguideSec").style.height = "100%";
+}
+
+function closeStyleguide() {
+  document.getElementById("styleguideSec").style.height = "0%";
+}
+
+//wireframe opening
+const trigWireframe = document.querySelector("#wireframe");
+const closerWireframe = document.querySelector("#wireframeSec");
+
+trigWireframe.addEventListener("click", openWireframe);
+closerWireframe.addEventListener("click", closeWireframe);
+
+function openWireframe() {
+  document.getElementById("wireframeSec").style.height = "100%";
+}
+
+function closeWireframe() {
+  document.getElementById("wireframeSec").style.height = "0%";
+}
+
+//XD prototype opening
+
+const trigPrototype = document.querySelector("#prototype");
+const closerPrototype = document.querySelector("#prototypeSec");
+
+trigPrototype.addEventListener("click", openPrototype);
+closerPrototype.addEventListener("click", closePrototype);
+
+function openPrototype() {
+  document.getElementById("prototypeSec").style.height = "100%";
+}
+
+function closePrototype() {
+  document.getElementById("prototypeSec").style.height = "0%";
+}
+
+
+//Mobile sketches opening
+const trigMobile = document.querySelector("#mobile");
+const closerMobile = document.querySelector("#mobileSec");
+
+trigMobile.addEventListener("click", openMobile);
+closerMobile.addEventListener("click", closeMobile);
+
+function openMobile() {
+  document.getElementById("mobileSec").style.height = "100%";
+}
+
+function closeMobile() {
+  document.getElementById("mobileSec").style.height = "0%";
+}
